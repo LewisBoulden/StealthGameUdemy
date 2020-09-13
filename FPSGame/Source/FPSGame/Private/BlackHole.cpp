3065d6284eef36fb13df1a962fb9f18b9abe6e2e
Jason Howard <jasonh78@gmail.com>
2020-09-13 12:08:43 -0700

    Implemented Black Hole from challenges. Changing file structure to allow for more projects for this course in one depot.

---
+++ FPSGame/Source/FPSGame/Private/BlackHole.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlackHole.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+ABlackHole::ABlackHole()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    MeshComponent->SetRelativeScale3D(FVector(6.f, 6.f, 6.f));
+	RootComponent = MeshComponent;
+
+	SuctionSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SuctionSphereComponent"));
+	SuctionSphereComponent->SetSphereRadius(SuctionSphereDefaultRadius);
+	SuctionSphereComponent->SetupAttachment(MeshComponent);
+
+	DestructionSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("DestructionSphereComponent"));
+	DestructionSphereComponent->SetSphereRadius(DestructionSphereDefaultRadius);
+	DestructionSphereComponent->SetupAttachment(MeshComponent);
+}
+
+
+// Called every frame
+void ABlackHole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+    HandleBlackholeSuction();
+}
+
+
+// Called when the game starts or when spawned
+void ABlackHole::BeginPlay()
+{
+    Super::BeginPlay();
+
+    DestructionSphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ABlackHole::OverlapDestructionSphere);
+}
+
+
+bool ABlackHole::GetOverlappingComps(const USphereComponent& Comp)
+{
+    OverlappingComps.Empty();
+    SuctionSphereComponent->GetOverlappingComponents(OverlappingComps);
+    
+    return (OverlappingComps.Num() > 0) ? true : false;
+}
+
+
+void ABlackHole::HandleBlackholeSuction()
+{
+    auto AreCompsOverlapping = GetOverlappingComps(*SuctionSphereComponent);
+    if (AreCompsOverlapping == true)
+    {
+        for (const auto attractedComponenet : OverlappingComps)
+        {
+            if (attractedComponenet != nullptr && attractedComponenet->IsSimulatingPhysics())
+            {
+                attractedComponenet->AddRadialForce(
+                    GetActorLocation(),
+                    SuctionRadius,
+                    SuctionStrength,
+                    ERadialImpulseFalloff::RIF_Linear
+                );
+            }
+        }
+    }
+}
+
+void ABlackHole::OverlapDestructionSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (OtherActor != nullptr)
+    {
+        OtherActor->Destroy();
+    }
+}
+
---
+++ FPSGame/Source/FPSGame/Public/BlackHole.h
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GenericPlatform/GenericPlatformMath.h"
+#include "BlackHole.generated.h"
+
+class UStaticMeshComponent;
+class USphereComponent;
+
+UCLASS()
+class FPSGAME_API ABlackHole : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABlackHole();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+private:
+	bool GetOverlappingComps(const USphereComponent& Comp);
+	void HandleBlackholeSuction();
+
+	UFUNCTION()
+	void OverlapDestructionSphere(UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+		bool bFromSweep, const FHitResult& SweepResult);
+
+protected:
+	
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	USphereComponent* SuctionSphereComponent;
+	
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	USphereComponent* DestructionSphereComponent;
+
+	UPROPERTY(BlueprintReadWrite, Category="Gameplay")
+    float SuctionSphereDefaultRadius = 3 * 1024.f;
+
+	UPROPERTY(BlueprintReadWrite, Category="Gameplay")
+    float DestructionSphereDefaultRadius = 20.f;
+
+	UPROPERTY(BlueprintReadWrite, Category="Gameplay")
+    float SuctionRadius = 3 * 1024.f;
+
+	UPROPERTY(BlueprintReadWrite, Category="Gameplay")
+    float SuctionStrength = 3 * -FMath::Pow(10, 6.3f);
+
+
+private:
+	TArray<UPrimitiveComponent*> OverlappingComps;
+
+};
