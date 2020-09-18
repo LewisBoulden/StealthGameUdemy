e87f1f780c075a6ff61ae5ffcb0b09a431ae1649
Jason Howard <jasonh78@gmail.com>
2020-09-18 15:26:14 -0700

    Formatting.

--- FPSGame/Source/FPSGame/Private/BlackHole.cpp
+++ FPSGame/Source/FPSGame/Private/BlackHole.cpp
@@ -74,7 +74,8 @@ void ABlackHole::HandleBlackholeSuction()
     }
 }
 
-void ABlackHole::OverlapDestructionSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void ABlackHole::OverlapDestructionSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
     if (OtherActor != nullptr)
     {
